455de8dece044240cdc7090a5485ef2c43f7e087
BrMarwa <cmarwabriki@gmail.com>
2019-03-20 14:29:33 +0100

    ajout des méthodes de manipulation des thèmes

--- src/AppBundle/Controller/ForumController.php
+++ src/AppBundle/Controller/ForumController.php
@@ -6,15 +6,107 @@ use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
+use AppBundle\Entity\Theme;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
 
 class ForumController extends Controller
 {
     /**
-     * @Route("/accueil", name="accuiel")
+     * @Route("/forum", name="accuiel")
      * @Template("@App/forum/index.html.twig")
-     */
+     */ 
     public function indexAction(Request $request)
     {
+        $listThemes = $this->getDoctrine()
+        ->getManager()
+        ->getRepository('AppBundle:Theme')->findAll();
+        //var_dump($listThemes);
+        return array(
+            'listThemes' => $listThemes
+        );
+    }
+
+    /**
+     * @Route("/forum/add", name="ajout")
+     * @Template("@App/forum/view.html.twig")
+     */
+    public function addAction(Request $request)
+    {
+        $theme = new Theme();
+        $theme->setIntitule('Matériel');
+
         
+        $em = $this->getDoctrine()->getManager();
+        $em->persist($theme);
+        $em->flush();
+        
+        return $this->redirectToRoute('affichage', array('id' => $theme->getId()));
+    }
+
+    /**
+     * @Route("/forum/view/{id}", defaults={"id" = 0}, name="affichage")
+     * @Method("GET")
+     * @Template("@App/forum/view.html.twig")
+     */
+    public function viewAction($id)
+    {
+        
+        $themes = $this->getDoctrine()
+        ->getManager()
+        ->getRepository('AppBundle:Theme')
+         ->getThemeById($id)
+       ;
+        return array(
+            'themes' => $themes
+        );
+        
+        
+    }
+
+    /**
+     * @Route("/forum/edit/{id}", defaults={"id" = 0}, name="modifier")
+     * @Method("GET")
+     * @Template("@App/forum/edit.html.twig")
+     */
+    public function editAction($id, Request $request)
+    {
+        // il faut ajouter un formulaire
+        $themes = $this->getDoctrine()
+        ->getManager()
+        ->getRepository('AppBundle:Theme')
+         ->getThemeById($id)
+       ;
+        return array(
+            'themes' => $themes
+        );
+    }
+
+    /**
+     * @Route("/forum/delete/{id}", defaults={"id" = 0}, name="supprimer")
+     * @Method("GET")
+     * @Template("@App/forum/delete.html.twig")
+     */
+    public function deleteAction($id, Request $request)
+    {
+        
+        
+        $em = $this->getDoctrine() ->getManager();
+        $themes = $em->getRepository('AppBundle:Theme') ->find($id);
+        
+     if (null === $themes) {
+        throw new NotFoundHttpException("Le thème d'id ".$id." n'existe pas.");
+      }
+      // On boucle sur les messages du thème pour les supprimer
+     foreach ($themes->getMessages() as $message) {
+        $themes->removeMessage($message);
+      }
+      $em->remove($themes);
+      $em->flush();
+
+      $request->getSession()->getFlashBag()->add('info', "Le thème a bien été supprimée.");
+
+      return $this->redirectToRoute('accuiel');
+
+
     }
 }
---
+++ src/AppBundle/Entity/Theme.php
@@ -0,0 +1,146 @@
+<?php
+
+namespace AppBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Validator\Constraints as Assert;
+use Doctrine\Common\Collections\ArrayCollection;
+
+/**
+ * Theme
+ *
+ * @ORM\Table(name="theme")
+ * @ORM\Entity(repositoryClass="AppBundle\Repository\ThemeRepository")
+ */
+class Theme
+{
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="intitule", type="string", length=255, unique=true)
+     */
+    private $intitule;
+
+     /**
+     * @var \DateTime
+     *
+     * @ORM\Column(name="date", type="datetime")
+     * @Assert\DateTime()
+     */
+    private $date;
+
+    /**
+     * @ORM\OneToMany(targetEntity="AppBundle\Entity\Message", mappedBy="theme", cascade={"persist", "remove"})
+     */
+    private $messages;
+
+
+    public function __construct()
+    {
+        $this->date = new \Datetime();
+        $this->messages = new ArrayCollection(); 
+
+    }
+    /**
+     * Get id
+     *
+     * @return int
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set intitule
+     *
+     * @param string $intitule
+     *
+     * @return Theme
+     */
+    public function setIntitule($intitule)
+    {
+        $this->intitule = $intitule;
+
+        return $this;
+    }
+
+    /**
+     * Get intitule
+     *
+     * @return string
+     */
+    public function getIntitule()
+    {
+        return $this->intitule;
+    }
+
+    /**
+     * Set date
+     *
+     * @param \DateTime $date
+     *
+     * @return Theme
+     */
+    public function setDate($date)
+    {
+        $this->date = $date;
+
+        return $this;
+    }
+
+    /**
+     * Get date
+     *
+     * @return \DateTime
+     */
+    public function getDate()
+    {
+        return $this->date;
+    }
+
+    /**
+     * Add message
+     *
+     * @param \AppBundle\Entity\Message $message
+     *
+     * @return Theme
+     */
+    public function addMessage(\AppBundle\Entity\Message $message)
+    {
+        $this->messages[] = $message;
+        // On lie le message au thème 
+        $message->setTheme($this);
+
+        return $this;
+    }
+
+    /**
+     * Remove message
+     *
+     * @param \AppBundle\Entity\Message $message
+     */
+    public function removeMessage(\AppBundle\Entity\Message $message)
+    {
+        $this->messages->removeElement($message);
+    }
+
+    /**
+     * Get messages
+     *
+     * @return \Doctrine\Common\Collections\Collection
+     */
+    public function getMessages()
+    {
+        return $this->messages;
+    }
+}
